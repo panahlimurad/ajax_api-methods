154652693c9163db0c8ee31facf4af4590fac88c
panahlimurad <panahli.murad@gmail.com>
2023-06-10 17:55:27 +0400

    creat new function

--- app.js
+++ app.js
@@ -1,96 +1,87 @@
 $(document).ready(function () {
     
 
-    const url = "https://blog-api-t6u0.onrender.com/posts"
+
+  function showUpdateData() {
     
-    // GET METHOD
+    $.ajax({
+      url: "https://blog-api-t6u0.onrender.com/posts",
+      method: "GET",
+      success: function (data) {
+        showAllData(data)
+        console.log(data);
+      },
+      error: function () {
+        console.log("Not info");
+      },
+    });
+    
+  }
+  
+  function showAllData(item) {
+    const cardsHTML = item.reverse().map(function (item) {
+       return `
+        <div class="form-group">
+                        <textarea class="form-control border-primary" id="exampleFormControlTextarea1" rows="3">${item.title}</textarea>
+                        <button class="button-delete" value="${item.id}"><i class="fa-solid fa-trash text-danger mt-3"></i></button>
+                    </div>`;
+      
+    }).join("")
 
-           $.ajax({
-             url: url,
-             method: "GET",
-             success: function (data) {
-               printText(data);
-               console.log(data);
-             },
-             error: function (error) {
-               console.log(error);
-             },
-           });
+      $("#main").html(cardsHTML);
+  }
+  
+  
+  $(document).on("click", ".button-delete", function () {
+    let id = $(this).val();
+    deleteItem(id);
+    showUpdateData();
+    console.log("id", id);
+  });
+  
+  
+  function deleteItem(id) {
+    $.ajax({
+      url: `https://blog-api-t6u0.onrender.com/posts/${id}`,
+      method: "DELETE",
+      ContentType: "application/json",
+      success: function (data) {
+        console.log(data);
+      },
+      error: function (error) {
+        console.log(error);
+      },
+    });
+    
+  }
+  
+  function postData(title) {
+    
 
+            $.ajax({
+              url: "https://blog-api-t6u0.onrender.com/posts",
+              method: "POST",
+              data: { title : title},
+              ContentType: "application/json",
+              success: function (data) {
+                showUpdateData()
+                console.log(data);
+              },
+              error: function (error) {
+                console.log(error);
+              },
+            });
+  }
+  
 
-    // POST METHOD
+  $(document).on("click", "#buttonAdd", function () {
+    let title = $("#text").val()
+    postData(title)
+    $("#text").val("");
+  });
+  showUpdateData()
+  
 
-
-    // PUT METHOD
-
-    // $.ajax({
-    //   url: url+ "/75",
-    //   method: "PUT",
-    //   data: JSON.stringify({ data: "updated" }),
-    //     ContentType: "application/json",
-    //     success: function (data) {
-    //       console.log(data);
-    //     },
-    //     error: function (error) {
-    //         console.log(error);
-    //     }
-    // });
-
-    // DELETE METHOD
-
-    // $.ajax({
-    //   url: url + "/85",
-    //   method: "DELETE",
-    //   ContentType: "application/json",
-    //   success: function (data) {
-    //     console.log(data);
-    //   },
-    //   error: function (error) {
-    //     console.log(error);
-    //   },
-    // });
-
-
-
-    $(document).on("click", "#buttonAdd", function () {
-        
-        let formData = {
-            text: $("#text").val()
-        }
-
-        console.log(formData);
-
-        
-
-            // $.ajax({
-            //   url: url,
-            //   method: "POST",
-            //   data: formData,
-            //   ContentType: "application/json",
-            //     success: function (data) {
-            //         $("textarea").text(JSON.stringify(data.text));
-            //         $("#main").append(printText(data.text))
-            //     console.log(data);
-            //   },
-            //   error: function (error) {
-                //     console.log(error);    
-                //   },
-                // });
-                
-                
-            })    
-            
-            
-             function printText(text) {
-     
-                 
-     
-                 return $("#main").html(`
-                         <textarea class="form-control border-primary" id="exampleFormControlTextarea1" rows="3">${text}</textarea>
-                         <i class="fa-solid fa-trash text-danger mt-3" style="cursor: pointer;"></i>
-                         <i class="fa-solid fa-pen-to-square ml-4 text-warning" style="cursor: pointer;"></i>`);
-             }            
-     
              
     
 
